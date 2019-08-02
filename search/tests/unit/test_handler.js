121c3ec5853033160f69c7b13068cb73853fd03f
Pete Shepley <peter.shepley@gmail.com>
2019-08-02 09:48:59 -0400

    Add eslint to package.json; lint js

--- search/tests/unit/test_handler.js
+++ search/tests/unit/test_handler.js
@@ -5,20 +5,18 @@ const chai = require('chai');
 const expect = chai.expect;
 var event, context;
 
-
 describe('Tests index', function () {
-    it('verifies successful response', async () => {
-        const result = await app.lambda_handler(event, context, (err, result) => {
-            expect(result).to.be.an('object');
-            expect(result.statusCode).to.equal(200);
-            expect(result.body).to.be.an('string');
+  it('verifies successful response', async () => {
+    const result = await app.lambda_handler(event, context, () => {
+      expect(result).to.be.an('object');
+      expect(result.statusCode).to.equal(200);
+      expect(result.body).to.be.an('string');
 
-            let response = JSON.parse(result.body);
+      const response = JSON.parse(result.body);
 
-            expect(response).to.be.an('object');
-            expect(response.message).to.be.equal("hello world");
-            expect(response.location).to.be.an("string");
-        });
+      expect(response).to.be.an('object');
+      expect(response.message).to.be.equal('hello world');
+      expect(response.location).to.be.an('string');
     });
+  });
 });
-
