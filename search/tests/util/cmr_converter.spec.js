3fed3529a4bbea423b5c949ced6b3b72a8f5938b
marquet <marquet@element84.com>
2019-08-20 18:11:12 -0400

    [staccmr-21] finished writing test for cmr_converter

--- search/tests/util/cmr_converter.spec.js
+++ search/tests/util/cmr_converter.spec.js
@@ -93,7 +93,7 @@ describe('cmrGranToFeatureGeoJSON', () => {
       }
     ],
     data_center: 'USA',
-    points: ['39, 77']
+    points: ['39,77']
   }
 
   const event = { headers: { Host: 'example.com' }, queryStringParameters: [] };
@@ -102,37 +102,90 @@ describe('cmrGranToFeatureGeoJSON', () => {
     expect(cmrGranToFeatureGeoJSON(event, cmrGran)).toEqual({
       type: 'Feature',
       id: 1,
-      geometry: { type: 'Point', coordinates: [39, 77] },
+      geometry: { type: 'Point', coordinates: [77, 39] },
+      properties: {
+        provider: 'USA',
+        datetime: '0/1'
+      },
+      assets: {},
       links: {
         self: {
           rel: 'self',
-          href: 'http://example.com/collections/10/items/1 '
+          href: 'http://example.com/collections/10/items/1'
         },
         parent: {
           rel: 'parent',
           href: 'http://example.com/collections/10'
         },
         metadata: {
-          href: 'http://example.com/concepts/1.native',
+          href: 'https://cmr.earthdata.nasa.gov/search/concepts/1.native',
           rel: 'metadata',
           type: 'application/json',
-          title: ''
-        },
-        properties: {
-          provider: 'USA',
-          datetime: '0/1'
-        },
-        assets: {
-          name: 'Info about this collection',
-          href: 'http://example.com/collections/id',
-          type: 'application/json' 
+          title: undefined
         }
       }
     })
   })
 });
 
-describe.skip('cmrGranulesToFeatureCollection', () => {
+describe('cmrGranulesToFeatureCollection', () => {
+  const cmrGran = [{
+    id: 1,
+    collection_concept_id: 10,
+    dataset_id: 'datasetId',
+    summary: 'summary',
+    time_start: 0,
+    time_end: 1,
+    links: [
+      {
+        href: 'http://example.com/collections/id',
+        rel: 'self',
+        title: 'Info about this collection',
+        type: 'application/json'
+      }
+    ],
+    data_center: 'USA',
+    points: ['39,77']
+  }]
+
+  const event = { headers: { Host: 'example.com' }, queryStringParameters: [] };
+
+  it('should return a cmrGranule to a FeatureCollection', () => {
+    expect(cmrGranulesToFeatureCollection(event, cmrGran)).toEqual({
+      type: 'FeatureCollection', 
+      features: [{
+        id: 1,
+        geometry: { type: 'Point', coordinates: [77, 39] },
+        properties: {
+          provider: 'USA',
+          datetime: '0/1'
+        },
+        type: 'Feature',
+        assets: {},
+        links: {
+          self: {
+            rel: 'self',
+            href: 'http://example.com/collections/10/items/1'
+          },
+          parent: {
+            rel: 'parent',
+            href: 'http://example.com/collections/10'
+          },
+          metadata: {
+            href: 'https://cmr.earthdata.nasa.gov/search/concepts/1.native',
+            rel: 'metadata',
+            type: 'application/json',
+            title: undefined
+          }
+        }
+      }],
+    links: {
+      self: 'http://example.com/undefined?',
+      next: 'http://example.com/undefined?page_num=2'
+    }
+    })
+  })
+
 });
 
 describe('parseOrdinateString', () => {
