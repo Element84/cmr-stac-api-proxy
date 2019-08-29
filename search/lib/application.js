62a47a0cf2f1283f18376f4a742d4a5372a03c51
Peter Shepley <peter@element84.com>
2019-08-29 15:14:07 -0400

    Moving collections endpoints

--- search/lib/application.js
+++ search/lib/application.js
@@ -4,10 +4,9 @@ const api = require('./api');
 async function initialize () {
   const application = express();
 
-  application.use(api.stac);
   application.use(api.wfs);
 
   return application;
 }
 
-module.export = initialize;
+module.exports = initialize;
--- search/lib/convert/granules.js
+++ search/lib/convert/granules.js
@@ -1,3 +1,4 @@
+
 const _ = require('lodash');
 const cmr = require('../cmr/cmr');
 const { pointStringToPoints, parseOrdinateString } = require('./bounding-box');
