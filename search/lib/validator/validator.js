b14cad837a79e824f80fa3e31eed0afc2fad566b
marquet <marquet@element84.com>
2019-09-03 14:59:48 -0400

    [staccmr-41] small initial setup

--- search/lib/validator/validator.js
+++ search/lib/validator/validator.js
@@ -3,11 +3,10 @@ const yaml = require('js-yaml');
 const Ajv = require('ajv');
 const ajv = new Ajv({ allErrors: true });
 
-const swaggerFileContents = fs.readFileSync('docs/WFS3core+STAC.yaml');
+const swaggerFileContents = fs.readFileSync('../docs/WFS3core+STAC.yaml');
 const swagger = yaml.safeLoad(swaggerFileContents);
 
 const createSchemaValidator = (schemaElement) => {
-  // TODO: remove lodash
   const schema = Object.assign({}, { components: swagger.components }, swagger.components.schemas[schemaElement]);
   return ajv.compile(schema);
 };
