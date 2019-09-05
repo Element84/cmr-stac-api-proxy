b411ae2ee4fe1ef61634ca9c488a8147c146f75b
marquet <marquet@element84.com>
2019-09-05 11:16:41 -0400

    Revert "test"
    This reverts commit cff8ad90fd47f07cef6f8d87adca733dea485441.

--- search/tests/validator/validator.spec.js
+++ search/tests/validator/validator.spec.js
@@ -1,13 +1,11 @@
-const { createSchemaValidator } = require('../../lib/validator/validator');
+const {createSchemaValidator} = require('../../lib/validator/validator')
 
 describe('validator', () => {
   it.skip('should validate a swagger components', () => {
-    expect(createSchemaValidator()).toEqual({});
-  });
+    expect(createSchemaValidator()).toEqual({})
+  })
 
   it.skip('should validate a schema with a schema element', () => {
-    expect(createSchemaValidator()).toEqual();
-  });
-});
-
-// test
+    expect(createSchemaValidator()).toEqual()
+  })
+})
